e94647c2c02277d6e55ba7d91f8bd73729486000
Edily Mora <edilyrosa@gmail.com>
2024-06-13 18:45:42 -0400

    "Updated login functionality to use Next-Auth credentials provider with bcrypt password hashing and schema validation"

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,27 @@
-import GitHub from "next-auth/providers/github";
+import bcrypt from "bcryptjs";
 import type { NextAuthConfig } from "next-auth";
-import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { LoginSchema } from "@/schemas"
+import { getUserEmail } from "@/data/user";
 
 export default {
-    providers: [GitHub],
+    providers: [
+        Credentials({
+            async authorize(credentials){
+                const validateFields = LoginSchema.safeParse(credentials)
+                if(validateFields.success) {
+                    const {email, password} = validateFields.data
+                    const user = await getUserEmail(email)
+                        if(!user || !user.password) return null
+
+                        const passwordsMatch = await bcrypt.compare(
+                            password,
+                            user.password
+                        );
+                        if(passwordsMatch) return user
+                }
+                return null
+            }
+        })
+    ],
     } satisfies NextAuthConfig
